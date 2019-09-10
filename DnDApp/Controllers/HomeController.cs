6a5ed5fc85ca7125d0f90530f7d70a55310e8731
Sean <seankeckler@gmail.com>
2019-09-09 20:10:17 -0400

    initial commit

---
+++ DnDApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DnDApp.Models;
+
+namespace DnDApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //have the monsters have a full stat card instead of entering the passive perception and stealth and other random stuff
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Monsters()
+        {
+
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DnDApp/Models/Monsters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnDApp.Models
+{
+
+
+    public class Monsters
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public string Type { get; set; }
+        public string Subtype { get; set; }
+        public string Alignment { get; set; }
+        public int AC { get; set; }
+        public int HP { get; set; }
+        public string HitDie { get; set; }
+        public string Speed { get; set; }
+        public int Str { get; set; }
+        public int Dex { get; set; }
+        public int Con { get; set; }
+        public int Int { get; set; }
+        public int Wis { get; set; }
+        public int Cha { get; set; }
+
+        public Monsters()
+        {
+
+        }
+        
+        public Monsters(string Menu, string Size, string Type, string Subtype, string Alignment, string AC, string HP, String HitDie,
+          string Speed, string Str, string Dex, string Con, string Int, string Wis, string Cha)
+        {
+
+        }
+    }
+    
+
+
+
+}
